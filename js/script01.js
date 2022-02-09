5fc0e92a9b8c269fb86f5b193c14d81ac4c1fe6a
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2022-02-08 22:52:30 -0500

    version 0.0.4

--- js/script01.js
+++ js/script01.js
@@ -6,3 +6,9 @@ window.onmousemove = (e) => {
     cursor.style.top = e.pageY + 'px';
     cursor.style.left = e.pageX + 'px';
 }
+
+document.querySelectorAll('.navbar a').forEach(link =>{
+    link.onmouseenter = () =>{
+        document.querySelector('.navbar-img img').src = link.getAttribute('data-src');
+    }
+})

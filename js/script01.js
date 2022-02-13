0ed3421f722c523941d52449a726c13799b28dd1
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2022-02-13 10:45:35 -0500

    version 0.0.5

--- js/script01.js
+++ js/script01.js
@@ -10,5 +10,18 @@ window.onmousemove = (e) => {
 document.querySelectorAll('.navbar a').forEach(link =>{
     link.onmouseenter = () =>{
         document.querySelector('.navbar-img img').src = link.getAttribute('data-src');
-    }
-})
+    };
+    link.onmouseleave = () =>{
+        document.querySelector('.navbar-img img').src = 'images/nav-img-1.jpg'; 
+    };
+    
+})    
+
+let navbar = document.querySelector('.navbar')
+let navbarImg = document.querySelector('.navbar-img')
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    navbarImg.classList.toggle('active');
+}
+ 
